3c39760b6df2623b5c7dbd8c5d8f778fa900bf8c
Igor Marlon <igormarlon.o@gmail.com>
2025-09-05 18:07:33 -0300

    feat: add initial code structure

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // Quadro para desenhar, do tamanho da janela do navegador
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // Pinta o fundo de preto a cada quadro
+  background(0);
+}
