21decc768f2960131ef91bfd6c209472c54cb680
Igor Marlon <igormarlon.o@gmail.com>
2025-09-05 18:27:19 -0300

    feat(audio): implement microphone audio capture using p5.AudioIn

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+let mic;
+
 function setup() {
   // Quadro para desenhar, do tamanho da janela do navegador
   createCanvas(windowWidth, windowHeight);
+
+  // Objeto de entrada de áudio
+  mic = new p5.AudioIn();
+
+  // Captura de áudio
+  mic.start();
+
 }
 
 function draw() {
